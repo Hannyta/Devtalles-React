26340a8a2d79c089a2bbf22725edce41e64f66e8
Hanny <Hannylinares@gmail.com>
2025-11-18 10:12:41 -0300

    LoginPage diseño sin Layaout

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,71 @@
-
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+      <Grid
+        className="box-shadow"
+        size={{ xs: 12, sm: 3 }}
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+
+        <form>
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12, sm: 12 }}>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="correo@ejemplo.com"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid size={{ xs: 12, sm: 12 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="********"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid size={{ xs: 12 }}>
+              <Button variant="contained" fullWidth sx={{ mt: 1 }}>
+                Ingresar
+              </Button>
+            </Grid>
+
+            <Grid size={{ xs: 12 }}>
+              <Button
+                variant="outlined"
+                fullWidth
+                startIcon={<Google />}
+                sx={{ mt: 0.1 }}
+              >
+                Google
+              </Button>
+            </Grid>
+
+            <Grid size={{ xs: 12 }} sx={{ mt: 1 }}>
+              <Link component={RouterLink} to="/register">
+                Crear una cuenta
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
