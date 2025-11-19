8e66a61a1275565116e981de7c247a79bb59a7bc
Hanny <Hannylinares@gmail.com>
2025-11-19 09:37:51 -0300

    AuthLayaout

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,27 +1,12 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid
-        className="box-shadow"
-        size={{ xs: 12, sm: 3 }}
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }}>
-          Login
-        </Typography>
-
-        <form>
+    <AuthLayout title="Login">
+      <form>
           <Grid container spacing={2}>
             <Grid size={{ xs: 12, sm: 12 }}>
               <TextField
@@ -65,7 +50,6 @@ export const LoginPage = () => {
             </Grid>
           </Grid>
         </form>
-      </Grid>
-    </Grid>
+    </AuthLayout>
   );
 };
