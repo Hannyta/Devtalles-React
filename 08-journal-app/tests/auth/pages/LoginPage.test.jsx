7a145a2c30f74c845f583ebf7ad81f5634b857f6
Hanny <hannylinares@gmail.com>
2026-03-02 09:46:51 -0300

    pruebas en loginPage

---
+++ 08-journal-app/tests/auth/pages/LoginPage.test.jsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { LoginPage } from '../../../src/auth/pages/LoginPage';
+import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from '../../../src/store/auth';
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer
+    }
+})
+
+describe('Pruebas en <LoginPage />', () => {
+
+    test('debe de mostrar el componente correctamente', () => {
+
+      render(
+        <Provider store={ store }> 
+            <LoginPage />
+        </Provider>
+      )
+
+    });
+
+});
